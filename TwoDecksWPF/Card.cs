03e52c13586ff0bcb304b7a817f4f779893b02c7
Brandon Sia <brandonsia@hotmail.com>
2022-07-19 23:07:28 +1000

    Add project files.

---
+++ TwoDecksWPF/Card.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwoDecksWPF
+{
+    class Card
+    {
+        public Values Value { get; private set; }
+        public Suits Suit { get; private set; }
+
+        public Card(Values value, Suits suit)
+        {
+            this.Suit = suit;
+            this.Value = value;
+        }
+        public string Name
+        {
+            get { return $"{Value} of {Suit}"; }
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+    }
+
+    enum Suits
+    {
+        Diamonds,
+        Clubs,
+        Hearts,
+        Spades,
+    }
+    enum Values
+    {
+        Ace = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 11,
+        Queen = 12,
+        King = 13,
+    }
+
+
+}
